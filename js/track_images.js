8b2bbe4ebc7da73801bfa5f1e0beb057dc167c37
SimonPHawketts <simon.hawketts@gmail.com>
2015-12-20 11:29:11 +0000

    Update track_images.js

--- js/track_images.js
+++ js/track_images.js
@@ -2,12 +2,17 @@
  * Created by simon on 20/12/2015.
  */
 jQuery(document).on( 'jp_carousel.selectSlide', '.jp-carousel-wrap', function( event, slides ) {
-	// This is just to show you what values get passed in.  Delete it before going to production.
-	if ( window.console ) {
-		console.log( this );
-		console.log( event );
-		console.log( slides );
-		console.log( slides[0] );
-	}
-	//Add piwik page tracking 
+        // This is just to show you what values get passed in.  Delete it before going to production.
+        if ( window.console ) {
+                //console.log( this );
+                //console.log( event );
+                //console.log( slides );
+                // console.log( slides[0] );
+        }
+        //Add piwik page tracking
+        var url=jQuery('div img').attr('src');
+        // console.log(url);
+        _paq.push(['setDocumentTitle', url]);
+        _paq.push(['trackPageView']);
+
 } )
